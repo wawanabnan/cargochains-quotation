4c1ffe1bd2a992fde11293180d1f305f5a6b1c15
bin abnan <wawanov@gmail.com>
2025-08-26 21:12:51 +0700

    styling admin lte dan penyesuaian table list quotation

--- config/settings.py
+++ config/settings.py
@@ -32,14 +32,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'sales' / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -47,6 +47,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
